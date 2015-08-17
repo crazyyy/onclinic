bed69a9fcd612f0d80237ee2591978c89b73933c
crazyyy <crazyyy@gmail.com>
2015-08-17 15:40:47 +0300

    category

--- wordpress/wp-content/themes/wp-onclinic/category.php
+++ wordpress/wp-content/themes/wp-onclinic/category.php
@@ -1,10 +1,21 @@
 <?php get_header(); ?>
-  <article>
+<div class="content static-page">
+  <div class="block cf">
+    <div class="cont-l">
+      <?php get_sidebar(); ?>
+    </div>
+    <!-- content -->
+    <div class="cont-r">
+      <div class="brcr">
+        <?php if (function_exists('dimox_breadcrumbs')) dimox_breadcrumbs(); ?>
+      </div>
+      <h1 class="cat-title"><?php the_category(', '); ?></h1>
+      <div class="s-p-content rubrika-text">
+        <?php get_template_part('loop'); ?>
 
-    <h1 class="cat-title inner-title"><?php _e( 'Categories for', 'wpeasy' ); the_category(', '); ?></h1>
-    <?php get_template_part('loop'); ?>
+      </div>
+    </div>
     <?php get_template_part('pagination'); ?>
-
-  </article>
-<?php get_sidebar(); ?>
+    <!-- end content -->
+  </div>
 <?php get_footer(); ?>
