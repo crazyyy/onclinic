b7655210dbbb71335b1b6adad3d0861c13fed53b
crazyyy <crazyyy@gmail.com>
2015-08-17 17:35:51 +0300

    simple

---
+++ wordpress/wp-content/themes/wp-onclinic/page-price.php
@@ -0,0 +1,20 @@
+<?php /* Template Name: Price Page */ get_header(); ?>
+<div class="content static-page">
+  <div class="block cf">
+    <div class="cont-l">
+      <?php get_sidebar(); ?>
+    </div>
+
+    <!-- content -->
+    <div class="cont-r">
+      <div class="brcr">
+        <?php if (function_exists('dimox_breadcrumbs')) dimox_breadcrumbs(); ?>
+      </div>
+      <h1><?php the_title(); ?></h1>
+      <div class="s-p-content">
+        <?php the_content(); ?>
+      </div>
+    </div>
+    <!-- end content -->
+  </div>
+<?php get_footer(); ?>
