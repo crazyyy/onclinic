ca7acfe5178b162fcc452bd4b126820a748446a4
crazyyy <crazyyy@gmail.com>
2015-08-19 12:09:05 +0300

    reviews

--- wordpress/wp-content/themes/wp-onclinic/taxonomy-reviews-category.php
+++ wordpress/wp-content/themes/wp-onclinic/taxonomy-reviews-category.php
@@ -1,4 +1,4 @@
-<?php /* Template Name: Reviews Page */ get_header(); ?>
+<?php get_header(); ?>
 <div class="content static-page">
   <div class="block cf">
     <div class="cont-l">
@@ -10,14 +10,14 @@
       <div class="brcr">
         <?php if (function_exists('dimox_breadcrumbs')) dimox_breadcrumbs(); ?>
       </div>
-      <h1><?php the_title(); ?></h1>
+      <h1><?php $term = get_term_by( 'slug', get_query_var( 'term' ), get_query_var( 'taxonomy' ) ); echo $term->name; ?></h1>
 
       <div class="otd-filter cf">
         <div class="col col6">
 
         <?php
 
-          $terms = get_terms( 'reviews_category' );
+          $terms = get_terms( 'reviews-category' );
           $x = 0;
           if ( ! empty( $terms ) && ! is_wp_error( $terms ) ){
              echo '<ul class="department">';
@@ -39,16 +39,16 @@
 
       <div class="comments">
         <ul>
-           <?php if (have_posts()): while (have_posts()) : the_post(); ?>
-          <li>
-            <div class="name">
-              <?php the_title(); ?>, <span class="date"><?php the_time('d F Y'); ?></span>
-            </div><!-- name -->
-            <div class="text">
-              <?php the_content(); ?>
-            </div><!-- text -->
-          </li>
-          <?php endwhile; wp_reset_query(); ?>
+          <?php if (have_posts()): while (have_posts()) : the_post(); ?>
+            <li>
+              <div class="name">
+                <?php the_title(); ?>, <span class="date"><?php the_time('d F Y'); ?></span>
+              </div><!-- name -->
+              <div class="text">
+                <?php the_content(); ?>
+              </div><!-- text -->
+            </li>
+          <?php endwhile; endif; ?>
         </ul>
 
         <?php get_template_part('pagination'); ?>
